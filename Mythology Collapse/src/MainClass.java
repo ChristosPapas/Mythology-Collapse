ab5fdbc0bdb14f61b5d2e3484b2acdd51f3e82ba
ChristosPapas <94298049+ChristosPapas@users.noreply.github.com>
2021-11-14 16:14:54 +0200

    Add files via upload

---
+++ Mythology Collapse/src/MainClass.java
@@ -0,0 +1,6 @@
+public class MainClass {
+	public static void main(String[] args) {
+		System.out.println("επέλεξε έναν από τους παρακάτω ήρωες");
+		System.out.println("1. Περσέας 2.Ηρακλής 3.Ιάσωνας ");
+	}
+}
