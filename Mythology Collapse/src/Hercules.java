d5ac8d2bcfabad2d71c6099b449be96cc2298148
kosmass2001 <t8200053@aueb.gr>
2021-11-21 14:54:55 +0200

    Update Hercules.java

--- Mythology Collapse/src/Hercules.java
+++ Mythology Collapse/src/Hercules.java
@@ -7,13 +7,13 @@ public class Hercules {
  * Μετά την πρώτη κατηγορία θα υπάρχουν υποκατηγορίες
  * a->a1->ak->ak0->ak9...
  */
-	public Hercules() {
+	public static void main(String []args) {
 		System.out.println("Η αρχή του παιχνιδιού...");
 		answer_given=q1();
 		answer_given=q2(answer_given);
 		System.out.println("Ιστορία");
 		System.out.println(answer_given);
-		/*answer_given=q3(answer_given);
+		/*answer_given=q3(	);
 		answer_given=q4(answer_given);
 		answer_given=q5(answer_given);
 		answer_given=q6(answer_given);
@@ -28,7 +28,9 @@ public class Hercules {
 	 */
 	private static String q1() {
 		String answer;
-		System.out.println("Ποιός ήταν ο πατέρας του Ηρακλή; \n 1)Δίας.\n 2)Αμφιτρύωνας.");
+		System.out.println("Ποιός ήταν ο πατέρας του Ηρακλή;\n "
+							+ "1)Δίας.\n "
+							+ "2)Αμφιτρύωνας.");
 		answer = in.next();
 		return answer;
 	}
@@ -36,35 +38,54 @@ public class Hercules {
 	private static String q2(String already_answered) {
 		String answer = null;
 		switch (already_answered){
+		
 		case "1":
 			System.out.println("Η Ήρα ζήλευε τον Δία και αν ναι πως αντέδρασε;\n "
-								+ "1)Ναι\n "
-								+ "2)Όχι");
+								+ "1)Ναι.\n "
+								+ "2)Όχι.");
 			answer = in.next();
 			
 			if (answer.equals ("1")) {
 				System.out.println("1)Η Ήρα έστειλα φίδια για να σκοτώσουν τον Ηρακλή.\n"
-								 + "2)Η Ήρα πήγε η Ίδια να σκοτώσει τον Ηρακλή.");
-				answer += in.next();
+								 + "2)Η Ήρα πήγε η ίδια να σκοτώσει τον Ηρακλή.");
+				answer = in.next();
+				if (answer.equals("1")) {
+					return answer;
+					
+				} else if (answer.equals("2")) {
+					System.out.println("1)Η Ήρα απέτυχε και δεν σταμάτησε να αναζητεί τρόπο εκδίκησης.\n "
+									 + "2)Η Ήρα πέτυχε τον στόχο της και ο Ηρακλής πέθανε.");
+					answer = in.next();
+					if (answer.equals("1")) {
+						return answer;
+						
+					} else if (answer.equals("2")) {
+						System.out.println("Ο Ηρακλής πέθανε πολύ νωρίς στην Ιστορία.");
+						reset(0,'a');
+					}
+				}
+				
+				
 			} else if (answer.equals ("2")){
-				System.out.println("Ο Ηρακλής δεν έμαθε ποτέ για τις πραγματικές του δυνάμεις");
-				answer += "2";
+				System.out.println("Δεν ζήλευε, οπότε ο Ηρακλής δεν έμαθε ποτέ τις πραγματικές του δυνάμεις.");
+				ordinaryPerson();
 			}		
 		break;
+		
 		case "2":
-			System.out.println("Δεν ζήλευε, οπότε ο Ηρακλής δεν έμαθε ποτέ τις πραγματικές του δυνάμεις.");
+			System.out.println("Ο Ηρακλής μεγάλωσε ως κοινός θνητός");
 			ordinaryPerson(); //Η Ιστορία με πατέρα τον Αμφιτρύωνα.
-			answer = already_answered;
+			answer =  "2";
 		break;
 		}
-		return already_answered + answer;
+		return answer;
 	}
 	
 	private static void ordinaryPerson() {
-		
+		System.out.println("\n\n\nγειαααα");
 	}
 	
 	private static void reset(int checkpoint, char answergiven) {
-		
+		System.out.println("\n\n\nγειαααα");
 	}
 }
