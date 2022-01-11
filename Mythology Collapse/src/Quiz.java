a77a3b72d2dc1f94b5801689c11dfe81d52b3476
IoannaNtousaki <92949690+IoannaNtousaki@users.noreply.github.com>
2022-01-11 20:39:06 +0200

    Update Quiz.java

--- Mythology Collapse/src/Quiz.java
+++ Mythology Collapse/src/Quiz.java
@@ -55,7 +55,7 @@ public class Quiz {
 				s1 = String.valueOf(System.out.printf("Πρέπει να πληκτρολογήσεις έναν ακέραιο μεταξύ 1 και %d.\nΠροσπάθησε ξανά!\n",noOfAnswers));
 				panel.setMessage(s1);
 			}
-		}while(ckeck == false);	
+		}while(check == false);	
 		answer = i;	//convert i value which contains the number of answer into an integer value
 	}
 	
