6dc4fc999cd2850848d455eecf1dcd180d213a80
IoannaNtousaki <92949690+IoannaNtousaki@users.noreply.github.com>
2022-01-11 20:29:57 +0200

    Update Quiz.java

--- Mythology Collapse/src/Quiz.java
+++ Mythology Collapse/src/Quiz.java
@@ -42,7 +42,7 @@ public class Quiz {
 			try {
 				ans = panel.getMessage(); //take the player's answer as a String value
 				boolean check = false;
-				while(i <= noOfAnswers && check == false){
+				while(i < noOfAnswers && check == false){
 					i++;
 					if (ans.equals(Integer.toString(i))){
 					check = true;	// when check becomes true we have found an integer value from 1 to noOfAnswers 
