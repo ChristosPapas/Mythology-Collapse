27f20be15f59d618563361d8acd422a064429bf7
ChristosPapas <94298049+ChristosPapas@users.noreply.github.com>
2022-01-16 12:41:50 +0200

    Update MainClass.java

--- Mythology Collapse/src/MainClass.java
+++ Mythology Collapse/src/MainClass.java
@@ -1,6 +1,5 @@
-public class MainClass {
-	public static void main(String[] args) {
-		System.out.println("επέλεξε έναν από τους παρακάτω ήρωες");
-		System.out.println("1. Περσέας 2.Ηρακλής 3.Ιάσωνας       ");
+public class Main {
+	public static void main (String [] args) {
+		new Game();
 	}
 }
