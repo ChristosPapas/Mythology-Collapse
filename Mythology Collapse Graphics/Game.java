4d5a38029297fc18ba5e81b6e5a1c0ff26036df8
ChristosPapas <94298049+ChristosPapas@users.noreply.github.com>
2022-01-18 21:56:09 +0200

    Create Game.java

---
+++ Mythology Collapse Graphics/Game.java
@@ -0,0 +1,14 @@
+public class Game {
+	public static void main (String [] args) {
+		ShowMessage test=new ShowMessage();
+		int option=test.getOption();
+		Graphics panel=new Graphics();
+		if(option == 1) {
+			new Perseas(panel);
+		}else if( option==2){
+			new  Odysseus(panel);
+		}else if (option == 3 ) {
+			new Hercules(panel);
+		}
+	}
+}
